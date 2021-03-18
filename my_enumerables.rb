24b002a89f0f13ee3dcbd643048bb42ff5c05ff4
Rolando Alvarez <rolandoalvarezfaye@gmail.com>
2021-03-18 16:09:42 -0400

    more linters

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -49,7 +49,7 @@ module Enumerable
 
   def my_map(proc = nil)
     arr_new = []
-    if block_given? then
+    if block_given?
       my_each { |i| arr_new << yield(i) }
     else
       my_each { |i| arr_new << proc.call(i) }
@@ -61,7 +61,6 @@ module Enumerable
     my_each { |i| initial_value = yield(initial_value, i) }
     initial_value
   end
-
 end
 
 def multiply_els(arr)
@@ -69,35 +68,31 @@ def multiply_els(arr)
 end
 
 # Testing
-#control = [10, 1, 2, 5, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1]
+# control = [10, 1, 2, 5, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1]
 
-#puts "my_each:"
-#control.my_each { |value| p value }
-#puts
-#puts "my_each with index:"
-#control.my_each_with_index { |i, ind| print ind.to_s + ":" + i.to_s + " " }
-#puts
-#puts "my_select:"
-#p control.my_select { |i| i < 11 }
-#puts "my_all?:"
-#p control.my_all? { |i| i < 15 }
-#puts "my_any?:"
-#p control.my_any? { |i| i < 15 }
-#puts "my_none?:"
-#p control.my_none? { |i| i < 15 }
-#puts "my_count:"
-#p control.my_count(1)
-#p control.my_count
-
-#my_proc = Proc.new { |i| i % 2 }
-
-#puts "my_map:"
-#p control.my_map(my_proc)
-#p control.my_map { |i| i * 2 }
-#p control.my_map { |i| i * 2 }.my_map(my_proc)
-
-#puts "my_inject:"
-#p control.my_inject(10) { |sum, i| sum + i }
-
-#puts "multiply_els:"
-#p multiply_els(control)
+# puts "my_each:"
+# control.my_each { |value| p value }
+# puts
+# puts "my_each with index:"
+# control.my_each_with_index { |i, ind| print ind.to_s + ":" + i.to_s + " " }
+# puts
+# puts "my_select:"
+# p control.my_select { |i| i < 11 }
+# puts "my_all?:"
+# p control.my_all? { |i| i < 15 }
+# puts "my_any?:"
+# p control.my_any? { |i| i < 15 }
+# puts "my_none?:"
+# p control.my_none? { |i| i < 15 }
+# puts "my_count:"
+# p control.my_count(1)
+# p control.my_count
+# my_proc = Proc.new { |i| i % 2 }
+# puts "my_map:"
+# p control.my_map(my_proc)
+# p control.my_map { |i| i * 2 }
+# p control.my_map { |i| i * 2 }.my_map(my_proc)
+# puts "my_inject:"
+# p control.my_inject(10) { |sum, i| sum + i }
+# puts "multiply_els:"
+# p multiply_els(control)
