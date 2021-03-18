9479db873261f78450832b2dde582f338501b126
Rolando Alvarez <rolandoalvarezfaye@gmail.com>
2021-03-18 11:22:55 -0400

    each with index

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -2,6 +2,10 @@ module Enumerable
   def my_each
 		self.length.times{|i| yield(self[i])}
 	end
+
+  def my_each_with_index
+    self.length.times{|i| yield(self[i], i)}
+  end
 end
 
 puts
@@ -11,3 +15,5 @@ puts "control array = #{control}"
 puts
 puts "my_each:"
 control.my_each { |i| print i.to_s + " " }
+puts "my_each:"
+control.my_each_with_index { |i, ind| print ind.to_s + ":" + i.to_s + " " }
