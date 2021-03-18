2fede97ab8d982c977beb377b151ef95eeaa342b
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-18 12:31:05 -0300

    Finish my_select

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,19 +1,27 @@
 module Enumerable
   def my_each
-		self.length.times{|i| yield(self[i])}
-	end
+    length.times { |i| yield(self[i]) }
+  end
 
   def my_each_with_index
-    self.length.times{|i| yield(self[i], i)}
+    length.times { |i| yield(self[i], i) }
+  end
+
+  def my_select
+    arr_new = []
+    length.times { |i| arr_new.push(self[i]) if yield(self[i]) }
+    arr_new
   end
 end
 
 puts
-puts "---CONTROL---"
+puts '---CONTROL---'
 control = [1, 2, 8, 5, 5]
 puts "control array = #{control}"
 puts
-puts "my_each:"
-control.my_each { |i| print i.to_s + " " }
-puts "my_each:"
-control.my_each_with_index { |i, ind| print ind.to_s + ":" + i.to_s + " " }
+puts 'my_each:'
+control.my_each { |i| print "#{i} " }
+puts 'my_each:'
+control.my_each_with_index { |i, ind| print "#{ind}:#{i} " }
+puts 'my_select:'
+p control.my_select { |i| i < 3 }
