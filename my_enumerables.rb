211a53eca79f61eaa0d8c9438e5614b771ba8c14
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-19 19:47:58 -0300

    Fixing linter errors

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -31,10 +31,7 @@ module Enumerable
     all = true
     if block_given?
       my_each { |i| break all = false unless yield(i) }
-    elsif !arg.nil?
-      all = get_instance(arg)
-    else
-      my_each { |i| break all = false if [false, nil].include?(i) }
+    else arg.nil? ? my_each { |i| break all = false if [false, nil].include?(i) } : all = get_instance(arg)
     end
     all
   end
@@ -54,9 +51,9 @@ module Enumerable
   def get_instance_any(arg)
     all = true
     if arg.instance_of?(Regexp)
-      my_each { |i| break all = false unless i.to_s.match(arg).nil?  }
+      my_each { |i| break all = false unless i.to_s.match(arg).nil? }
     elsif arg.is_a?(Class)
-      my_each { |i| break all = false if i.is_a?(arg)  }
+      my_each { |i| break all = false if i.is_a?(arg) }
     else
       my_each { |i| break all = false if i == arg }
     end
@@ -66,14 +63,10 @@ module Enumerable
   def my_any?(arg = nil)
     control = 0
     if block_given?
-      all = true
-      my_each { |i| break all = false unless yield(i) }
-      all
-    elsif !arg.nil?
-      all = !get_instance_any(arg)
-      return all
+      my_each { |i| control += 1 if yield(i) }
+    elsif arg.nil?
+      get_instance_any(arg)
     else
-      control = 0
       my_each { |i| control += 1 if [false, nil].include?(i) }
       control.zero?
     end
@@ -85,8 +78,7 @@ module Enumerable
     if block_given?
       my_each { |i| control += 1 unless yield(i) }
     elsif !arg.nil?
-      all = get_instance_any(arg)
-      return all
+      return get_instance_any(arg)
     else
       my_each { |i| control += 1 if i != false || !i.nil? }
     end
@@ -96,40 +88,32 @@ module Enumerable
   def my_count(arg = nil)
     count = 0
     if arg.nil?
-      if block_given?
-        my_each { |i| count += 1 if yield(i) }
-        count
-      else
-        count = size
-      end
+      block_given? ? my_each { |i| count += 1 if yield(i) } : count = size
     else
       my_each { |i| count += 1 if i == arg }
-      count
     end
     count
   end
 
   def my_map(proc = nil)
     arr_new = []
-    if !proc.nil?
-      my_each { |i| arr_new << proc.call(i) }
-    elsif block_given?
-      my_each { |i| arr_new << yield(i) }
+    if proc.nil?
+      block_given? ? my_each { |i| arr_new << yield(i) } : to_enum(:my_map, proc)
     else
-      to_enum(:my_map, proc)
+      my_each { |i| arr_new << proc.call(i) }
     end
     arr_new
   end
 
   def my_inject(initial = nil, sym = nil)
-    if (!initial.nil? && sym.nil?) && (initial.is_a?(Symbol) || initial.is_a?(String))
+    if sym.nil? && (initial.is_a?(Symbol) || initial.is_a?(String))
       sym = initial
       initial = nil
     end
-    if !block_given? && !sym.nil?
-      my_each { |item| initial = initial.nil? ? item : initial.send(sym, item) }
-    else
+    if block_given?
       my_each { |item| initial = initial.nil? ? item : yield(initial, item) }
+    else
+      my_each { |item| initial = initial.nil? ? item : initial.send(sym, item) }
     end
     initial
   end
@@ -140,37 +124,37 @@ def multiply_els(arr)
 end
 
 # # Testing
- control = [10, 1, 2, 5, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1 ]
- control2 = [10, 1, 2, 5, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1 ]
-  control3 = [nil, nil]
-  control4 = [1, 2]
-  control5 = [2,3,4]
-  test = ["a", "c"]
-  puts "my_each:"
-  p control.my_each { |value| value < 3 }
-  p control.each { |value| value < 3}
-  puts "my_each with index:"
-  hash = {a: 1, b: 2, c: 3, d:4, e:5 }
-  p hash.my_each_with_index { |k, v| print k.to_s + ":" + v.to_s + " " }
-  p hash.each_with_index { |k, v| print k.to_s + ":" + v.to_s + " " }
-  puts "my_select:"
-  p control.my_select
-  puts "my_all?:"
-  p control2.my_all?(Numeric)
-  p test.my_all?(String)
-  p test.my_all?(/a/)
-  puts "my_any?:"
-  p test.my_any?(/d/)
-  puts "my_none?:"
-  p test.my_none?(String)
+#  control = [10, 1, 2, 5, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1 ]
+#  control2 = [10, 1, 2, 5, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1 ]
+#   control3 = [nil, nil]
+#   control4 = [1, 2]
+#   control5 = [2,3,4]
+# test = ["1", 3]
+#   puts "my_each:"
+#   p control.my_each { |value| value < 3 }
+#   p control.each { |value| value < 3}
+#   puts "my_each with index:"
+#   hash = {a: 1, b: 2, c: 3, d:4, e:5 }
+#   p hash.my_each_with_index { |k, v| print k.to_s + ":" + v.to_s + " " }
+#   p hash.each_with_index { |k, v| print k.to_s + ":" + v.to_s + " " }
+#   puts "my_select:"
+#   p control.my_select
+# puts "my_all?:"
+# p control2.my_all?(Numeric)
+# p test.my_all?(String)
+# p test.my_all?(/a/)
+# puts "my_any?:"
+# p test.my_any?{|i| i < 3}
+# puts "my_none?:"
+# p test.my_none?(String)
 #  puts "my_count:"
-#  p control.my_count
-#  p control.my_count
-#   my_proc = Proc.new { |i| i + 1 }
-#   puts "my_map:"
-#   p control.my_map(my_proc){ |i| i * 2 }
-#   p (1..6).my_map { |i| i * 2 }
-#   p control.my_map { |i| i * 2 }.my_map(my_proc)
-#   p control.my_map
+#  p control.my_count{|i| i < 17}
+#  p control.count{|i| i < 17}
+# my_proc = Proc.new { |i| i + 1 }
+# puts "my_map:"
+# p control.my_map(my_proc){ |i| i * 2 }
+# p (1..6).my_map { |i| i * 2 }
+# p control.my_map { |i| i * 2 }.my_map(my_proc)
+# p control.my_map
 #  puts "my_inject:"
-#  p (2..5).my_inject("+")
+#  p (2..5).my_inject{|sum, i| i + 2}
